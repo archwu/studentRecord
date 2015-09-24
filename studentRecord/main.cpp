7217f7a89648239e7609e72f426eebaf4b4c864b
Yuecong Wu <Arch@dyn-72-33-94-217.uwnet.wisc.edu>
2015-09-24 00:44:22 -0500

    Initial Commit

---
+++ studentRecord/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  studentRecord
+//
+//  Created by Yuecong Wu on 15/9/24.
+//  Copyright © 2015年 Yuecong Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
